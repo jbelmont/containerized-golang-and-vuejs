d7080b88079745fc5019a216ac499aae3dd32b74
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2018-01-10 21:49:04 -0500

    Changes to travis and update files.

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -7,45 +7,13 @@ import (
 )
 
 func TestUsersIndex(t *testing.T) {
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(UsersIndex)
 
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
+	r := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, r)
 
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	// Check the response body is what we expect.
-	resp := rr.Body
-	if resp == nil {
-		t.Errorf("body should not be nil")
-	}
-}
-
-func TestGetUsers(t *testing.T) {
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(GetUsers)
-
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
-
-	if header := rr.Header().Get("Content-Type"); header != "application/json" {
-		t.Error("Content-Type Header should equal application/json")
+	if w.Code != http.StatusOK {
+		t.Errorf("Should receive a status code of 200 for the response : %v", w.Code)
 	}
 }
